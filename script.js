0f34ba42b3bcc7855abdcc002882fe1ad671ee88
PhungNV1 <phung145nv>
2022-11-21 17:13:08 +0700

    the first part

---
+++ script.js
@@ -0,0 +1,11 @@
+let functionSum = (a, b) => {console.log(a + b)};
+functionSum(1,2 );
+
+const infoPerson = {
+    firstName: "defunger",
+    lastName : "vann", 
+    age: 25,
+};
+
+var functionInfo = (infoPerson) => {return infoPerson.firstName + " " + infoPerson.lastName};
+console.log(functionInfo(infoPerson));
