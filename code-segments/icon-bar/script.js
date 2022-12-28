17f79019a4eb4b0f99b8bcf8793dc99fac7baf96
PhungNV1 <phung145nv>
2022-12-28 19:28:36 +0700

    hehe

---
+++ code-segments/icon-bar/script.js
@@ -0,0 +1,17 @@
+  
+// const arr_link = document.getElementsByTagName("a");
+
+// var total = 100;
+// for(var index = 0; index < arr_link.length; index++)
+// {
+//     arr_link[index].style.width = total / arr_link.length  + "%"; 
+// }
+
+// if (typeof window === "object") {
+//     console.log(2);
+//     // code is running in a browser environment
+//   } else {
+//     console.log(3);
+
+//     // code is running in a non-browser environment
+//   }
--- script.js
+++ script.js
@@ -1,11 +1,45 @@
-let functionSum = (a, b) => {console.log(a + b)};
-functionSum(1,2 );
+// let myPromise = new Promise(function(myResolve, myReject) {
+//   // "Producing Code" (May take some time)
+  
+//     myResolve(); // when successful
+//     myReject();  // when error
+//   });
+  
+//   // "Consuming Code" (Must wait for a fulfilled Promise)
+//   myPromise.then(
+//     function(value) { /* code if successful */ },
+//     function(error) { /* code if some error */ }
+//   );
 
-const infoPerson = {
-    firstName: "defunger",
-    lastName : "vann", 
-    age: 25,
-};
+// $(document).ready(function()
+// {
+//   $("p").hover(function()
+//   {
+//     $(this).css("background-color","green");
+// })
+// })
 
-var functionInfo = (infoPerson) => {return infoPerson.firstName + " " + infoPerson.lastName};
-console.log(functionInfo(infoPerson));
+// $( "li" ).hover(  
+//   function() {  
+//     $( this ).append( $( "<span> ***</span>" ) );  
+//   }, function() {  
+//     $( this ).find( "span:last" ).remove();  
+//   }  
+// );  
+// $( "li.fade" ).hover(function() {  
+//   $( this ).fadeOut( 100 );  
+//   $( this ).fadeIn( 500 );  
+// });
+
+
+// $(document).ready(function(){
+  
+// })
+
+// var count = document.getElementsByTagName("a").length;
+// var index = 0;
+// var total = 100;
+// for(index ; index < count.lengt; index++)
+// {
+//     document.getElementsByTagName("a")[index].style.width = total/count + "%";
+// }
